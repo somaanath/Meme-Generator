368380a189692a8f16d9f5d16f0f138e922d6bea
somaanath <somaanath123@gmail.com>
2024-10-01 03:49:20 +0000

    Form Created

--- src/components/MemeGenerator/index.js
+++ src/components/MemeGenerator/index.js
@@ -1,3 +1,14 @@
+import {Component} from 'react'
+import {
+  BgContainer,
+  CreateForm,
+  Heading,
+  Input,
+  Label,
+  Select,
+  Button,
+} from './styledComponents'
+
 const fontSizesOptionsList = [
   {
     optionId: '8',
@@ -29,3 +40,46 @@ const fontSizesOptionsList = [
   },
 ]
 // Write your code here
+class MemeGenerator extends Component {
+  state = {fontSize: fontSizesOptionsList[0].optionId}
+
+  submitForm = event => {
+    event.preventDefault()
+  }
+
+  onChangeFontValue = event => {
+    console.log(event.target.value)
+  }
+
+  render() {
+    const {fontSize} = this.state
+    console.log(typeof fontSize)
+    return (
+      <BgContainer>
+        <CreateForm onSubmit={this.submitForm}>
+          <Heading>Meme Generator</Heading>
+          <Label htmlFor="url">Image Url</Label> <br />
+          <Input type="text" placeholder="Enter the Image Url" id="url" />
+          <br />
+          <Label htmlFor="top">Top Text</Label> <br />
+          <Input type="text" placeholder="Enter the Top Text" id="top" />
+          <br />
+          <Label htmlFor="bottom">Bottom Text</Label> <br />
+          <Input type="text" placeholder="Enter the Bottom Text" id="bottom" />
+          <br />
+          <Label htmlFor="font">Font Size</Label> <br />
+          <Select name="font-size" id="font" onChange={this.onChangeFontValue}>
+            {fontSizesOptionsList.map(eachValue => (
+              <option key={eachValue.optionId} value={eachValue.optionId}>
+                {eachValue.displayText}
+              </option>
+            ))}
+          </Select>
+          <Button type="submit">Generate</Button>
+        </CreateForm>
+      </BgContainer>
+    )
+  }
+}
+
+export default MemeGenerator
--- src/components/MemeGenerator/styledComponents.js
+++ src/components/MemeGenerator/styledComponents.js
@@ -1,1 +1,50 @@
 // Style your components here
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const CreateForm = styled.form`
+  padding: 50px;
+`
+export const Heading = styled.h1`
+  color: #35469c;
+  text-align: center;
+`
+export const Label = styled.label`
+  color: #d7dfe9;
+`
+export const Input = styled.input`
+  border: 1px solid #d7dfe9;
+  padding: 10px;
+  border-radius: 4px;
+  outline: none;
+  width: 110%;
+  color: #5a7184;
+  margin-top: 10px;
+  margin-bottom: 15px;
+`
+
+export const Select = styled.select`
+  border: 1px solid #d7dfe9;
+  padding: 10px;
+  border-radius: 4px;
+  outline: none;
+  width: 110%;
+  color: #5a7184;
+  margin-top: 10px;
+  margin-bottom: 15px;
+`
+
+export const Button = styled.button`
+  border: none;
+  background-color: #0b69ff;
+  width: 60%;
+  border-radius: 6px;
+  padding: 10px;
+  color: #ffffff;
+`
